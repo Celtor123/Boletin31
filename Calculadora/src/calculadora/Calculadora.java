2569cfffa6177d971d8dcc78c0c9c917ec95fcea
Juan <Juan@DESKTOP-J5PU6RU>
2018-06-05 19:44:40 +0200

    Creando el JFrame y primer método.

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,11 @@
+
+package calculadora;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        calculadora1 a= new calculadora1();
+        a.setVisible(true);
+    }
+    
+}
