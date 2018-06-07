a2ec88973fb8775323b65083bedd76c7beb4fa4d
Juan <Juan@DESKTOP-J5PU6RU>
2018-06-07 18:47:00 +0200

    Facendo clases secundarias con métodos.

--- Boletin31/src/boletin31/Empregado.java
+++ Boletin31/src/boletin31/Empregado.java
@@ -19,18 +19,77 @@ String nome, apelidos,direccion,DNI;
         this.teléfono = teléfono;
         this.salario = salario;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getApelidos() {
+        return apelidos;
+    }
+
+    public void setApelidos(String apelidos) {
+        this.apelidos = apelidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public int getTeléfono() {
+        return teléfono;
+    }
+
+    public void setTeléfono(int teléfono) {
+        this.teléfono = teléfono;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
     public void imprimir(){
-        JOptionPane.showMessageDialog(null, "nome"+nome+" apelidos"+apelidos+" direccion"+direccion+" DNI"+DNI+" teléfono"+teléfono+" salario"+salario);
+        JOptionPane.showMessageDialog(null, "nome:"+nome+" apelidos:"+apelidos+" direccion:"+direccion+" DNI:"+DNI+" teléfono:"+teléfono+" salario:"+salario);
     }
 public void incrementarSalario(){
     
 }
-public class supervisor{
-    
+public class Supervisor extends Empregado{
+    String nome;
+    Supervisor(){
+    nome=JOptionPane.showInputDialog("Poña o nome do supervisor");
+    }
 }
  
     public static void main(String[] args) {
-     Empregado Emilio= new Empregado("Celso","Emilio_Ferreiro","Calle/Soubiñas,13","263792G",986326123,1200.03);
+     Empregado Emilio= new Empregado("Celso","Emilio_Ferreiro","Calle/Soubiñas/13","263792G",986326123,1200.03);
      Emilio.imprimir();
     }
     
--- Boletin31/src/boletin31/Xefe.java
+++ Boletin31/src/boletin31/Xefe.java
@@ -6,16 +6,16 @@ import javax.swing.JOptionPane;
 
 
 public class Xefe extends Empregado{
-  ArrayList <Object> a= new ArrayList<>();
+    private Despacho despacho;
+    private Secretario secretario;
+    private Coche coche;
+  ArrayList <Secretario> a= new ArrayList<>();
 
 public void incrementarSalario(){
         ano=Integer.parseInt(JOptionPane.showInputDialog("Cantos anos leva este xefe traballando"));
         this.salario=((salario*12)+(ano*20/100))/12;
     }
-public class coche{
-   String matricula,marca,modelo;
-   
-}
+
   }
 //Ten despacho, ten un secretario ao seu cargo, unha lista de vendedores ao seu cargo e ten coche da empresa (identificado pola matrícula, marca e modelo). Incrementa o seu salario un 20% anual
 //. Terá, polo menos, as seguintes funciones membro:
