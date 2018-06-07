c86a1c4d4d9bb76ab905be94ed4062aca5053a42
Juan <Juan@DESKTOP-J5PU6RU>
2018-06-07 16:23:32 +0200

    Creando las clases.

--- Boletin31/src/boletin31/Secretario.java
+++ Boletin31/src/boletin31/Secretario.java
@@ -9,13 +9,6 @@ package boletin31;
  *
  * @author Juan
  */
-public class Boletin31 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Secretario {
     
 }
